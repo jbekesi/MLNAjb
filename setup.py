ebd862ce1e66afda3a38c01acafe64af5fe05691
Golnaz Sarkar Farshi <golnaz.s.farshi@gmail.com>
2024-07-11 15:09:14 +0200

    comment all lines in the setup.py file

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    content= f.readlines()
-requirements= [x.strip() for x in content]
+# with open('requirements.txt') as f:
+#     content= f.readlines()
+# requirements= [x.strip() for x in content]
 
-setup (name= 'mlna',
-       version= '0.0.1',
-       description= 'the multilingual network analysis package',
-       install_requires= requirements,
-       packages= find_packages()
-       )
+# setup (name= 'mlna',
+#        version= '0.0.1',
+#        description= 'the multilingual network analysis package',
+#        install_requires= requirements,
+#        packages= find_packages()
+#        )
