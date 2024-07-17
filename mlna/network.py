47e2118f3f71e2bf6995af2712bc95a4146e3edd
Golnaz Sarkar Farshi <golnaz.s.farshi@gmail.com>
2024-07-17 17:30:32 +0200

    test network module again

--- mlna/network.py
+++ mlna/network.py
@@ -132,10 +132,10 @@ def visualize_network (text_df, entity_tags, user_ents=None, user_dict=None, cor
 
     if core:
         net.from_nx(nx.k_core(G))
-        net.save_graph(f'{title}.html')
+        #net.save_graph(f'{title}.html')
     else:
         net.from_nx(G)
-        net.save_graph(f'{title}.html')
+        #net.save_graph(f'{title}.html')
 
     # New code:
     html_filename = f'{title}.html'
