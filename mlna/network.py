b23a4166b8b13bc71f5a91a54d7d167eec68bdc0
Golnaz Sarkar Farshi <golnaz.s.farshi@gmail.com>
2024-07-17 18:09:34 +0200

    give up visualizing network in jupyter

--- mlna/network.py
+++ mlna/network.py
@@ -134,11 +134,9 @@ def visualize_network (text_df, entity_tags, user_ents=None, user_dict=None, cor
 
     if core:
         net.from_nx(nx.k_core(G))
-        net.show(f'{title}.html')
         net.save_graph(f'{title}.html')
     else:
         net.from_nx(G)
-        net.show(f'{title}.html')
         net.save_graph(f'{title}.html')
 
     html_content = net.generate_html(f'{title}.html')
