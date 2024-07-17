36b4e67229892b6f607400772a3f29ee9958b62d
Golnaz Sarkar Farshi <golnaz.s.farshi@gmail.com>
2024-07-17 14:33:48 +0200

    update app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+
+
+APP_TITLE = 'MLNA'
+APP_SUB_TITLE = 'The MultiLingual Network Analysis package'
+
+
 st.markdown(
     """
     Hello world! I am here.
@@ -9,17 +15,18 @@ st.markdown(
 
 
 def main():
-    st.title("Upload your text data frame.")
+
+    st.set_page_config(APP_TITLE)
+    st.markdown(f"##### {APP_SUB_TITLE}")
 
     # Prompt the user to upload a file
     uploaded_file = st.file_uploader("Choose a pickle file", type="pickle")
-
     if uploaded_file is not None:
         # Read the file into a DataFrame
         text_df = pd.read_pickle(uploaded_file)
 
         # Display the DataFrame
-        st.write("Data Frame:")
+        st.write("Uploaded data frame:")
         st.dataframe(text_df)  # Use st.dataframe() for a more interactive display
 
 if __name__ == "__main__":
