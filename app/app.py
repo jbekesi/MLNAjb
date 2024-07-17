5f8a6b333b5ce7b75ca370c6f77b64caa93cdeb8
Golnaz Sarkar Farshi <golnaz.s.farshi@gmail.com>
2024-07-17 14:10:52 +0200

    add upload dataframe to streamlit

--- app/app.py
+++ app/app.py
@@ -1,7 +1,26 @@
 import streamlit as st
+import pandas as pd
 
 st.markdown(
     """
     Hello world! I am here.
     """
 )
+
+
+def main():
+    st.title("Upload your text data frame.")
+
+    # Prompt the user to upload a file
+    uploaded_file = st.file_uploader("Choose a pickle file", type="pickle")
+
+    if uploaded_file is not None:
+        # Read the file into a DataFrame
+        text_df = pd.read_pickle(uploaded_file)
+
+        # Display the DataFrame
+        st.write("Data Frame:")
+        st.dataframe(text_df)  # Use st.dataframe() for a more interactive display
+
+if __name__ == "__main__":
+    main()
